d7a4e54eb6297af743818bf5de2dde2da5819688
Dakshanamurthy <113338215+Dakshanamurthy@users.noreply.github.com>
2023-06-19 14:26:04 +0530

    Update 2.gaussian blur.py

--- 2.gaussian blur.py
+++ 2.gaussian blur.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread("C:/Users/Roshan/OneDrive/Pictures/animals-1663825597439-9977.jpg")
+img = cv2.imread("C:/Users/daksh/OneDrive/Pictures/animals-1663825597439-9977.jpg")
 blur = cv2.GaussianBlur(img, (5, 5), 0)
 cv2.imshow("Original", img)
 cv2.imshow("Blurred", blur)
